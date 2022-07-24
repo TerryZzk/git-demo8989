04e75d92b34f94685381afd32c132b195f6bf527
TerryZhuang <952072998@qq.com>
2022-07-24 21:48:46 +0800

    第一次提交GitTest代码

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author 庄志康
+ * @create 2022-07-24 21:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!!!!");
+    }
+}
